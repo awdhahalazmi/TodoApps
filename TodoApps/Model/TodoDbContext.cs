671052ee822cf8b3a4430778a87e5ed595c1527b
awdah <awdah@LAPTOP-DH3O97S3>
2024-04-30 08:12:43 +0300

    Add project files.

---
+++ TodoApps/Model/TodoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApps.Model.Entity;
+
+namespace TodoApps.Model
+{
+    public class TodoDbContext: DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //   modelBuilder.Entity<TodoItem>().HasData();
+
+        }
+    }
+    
+}
